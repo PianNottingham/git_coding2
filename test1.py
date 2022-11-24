50e44b6fd87b8c5f98210315109bed91aa845d68
PianNottingham <pian.nottingham@gmail.com>
2022-11-24 09:04:08 +0700

    Update test1.py

--- test1.py
+++ test1.py
@@ -2,3 +2,8 @@ alpha1 = "abc"
 alpha2 = "def"
 print (alpha1, alpha2,sep="-",end="|")
 print("dlkfjeos sdflkjesldf ;sdif ;asfjie; sdfwei")
+print(alpha1,alpha2,sep="---",end="|")
+alpha3 = "sutham"
+alpha4 = "piansaen"
+
+print(alpha4,alpha3,sep=":::::",end="|")
